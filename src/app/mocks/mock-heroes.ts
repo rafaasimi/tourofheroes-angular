5a808a9a5b5b32f6a89486e5f6bbc8161d77574b
Rafael Simionato <rafaasimi@hotmail.com>
2022-05-19 11:17:53 -0300

    Criação de mock de herois para consumo do componente

---
+++ src/app/mocks/mock-heroes.ts
@@ -0,0 +1,24 @@
+import IHero from '../models/heroes.model';
+
+export const HEROES: IHero[] = [
+  {
+    id: 1,
+    name: 'Homem Aranha',
+  },
+  {
+    id: 2,
+    name: 'Batman',
+  },
+  {
+    id: 3,
+    name: 'Super Homem',
+  },
+  {
+    id: 4,
+    name: 'Lanterna Verde',
+  },
+  {
+    id: 5,
+    name: 'Hulk',
+  },
+];
