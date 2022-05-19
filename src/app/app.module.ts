06b743ec13efb8ea77982fea4abf00125cbca968
Rafael Simionato <rafaasimi@hotmail.com>
2022-05-19 13:59:43 -0300

    Importação do menu de navegação e Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationMenuTopComponent } from './components/navigation-menu-top/navigation-menu-top/navigation-menu-top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    NavigationMenuTopComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
