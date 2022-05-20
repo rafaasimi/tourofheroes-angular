63ce0efa2da8ec29c6901d29490e066b8bf3b74a
Rafael Simionato <rafaasimi@hotmail.com>
2022-05-20 15:12:31 -0300

    Adicionado evento de mensagem quando heroi é selecionado ou carregado

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import IHero from 'src/app/models/heroes.model';
 import { HeroService } from 'src/app/services/hero.service';
+import { MessageService } from 'src/app/services/message.service';
+import getCurrentDateAndTime from 'src/app/utils/getCurrentDateAndTime';
 
 @Component({
   selector: 'app-heroes',
@@ -11,18 +13,31 @@ export class HeroesComponent implements OnInit {
   heroesList: IHero[] = [];
   selectedHero?: IHero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
-    this.getHeroes()
+    this.getHeroes();
   }
 
   onSelect(hero: IHero): void {
-    this.selectedHero = hero
+    this.selectedHero = hero;
+    this.messageService.add(`[${getCurrentDateAndTime()}] - The hero "${hero.name} (${hero.id})" has been selected.`)
+
   }
 
   getHeroes(): void {
-    this.heroesList = this.heroService.getHeroes();
-  }
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes) => (this.heroesList = heroes));
 
+    // Outra forma de criar um Observable (Next, Error, Complete)
+    // this.heroService.getHeroes().subscribe({
+    //   next(x) { console.log('got value ' + x); },
+    //   error(err) { console.error('something wrong occurred: ' + err); },
+    //   complete() { console.log('done'); }
+    // });
+  }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { HEROES } from '../mocks/mock-heroes';
 import IHero from '../models/heroes.model';
+import getCurrentDateAndTime from '../utils/getCurrentDateAndTime';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
-  getHeroes(): IHero[] {
-    return HEROES
-  };
+  getHeroes(): Observable<IHero[]> {
+    const heroes = of(HEROES);
+
+    this.messageService.add(`[${getCurrentDateAndTime()}] - Hero list loaded.`)
+
+    return heroes;
+  }
 }
