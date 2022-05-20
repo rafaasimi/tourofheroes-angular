2d08207dc0de3c8b2c41460918a6eef15815386b
Rafael Simionato <rafaasimi@hotmail.com>
2022-05-20 15:13:37 -0300

    Importação do componente de mensagem e material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationMenuTopComponent } from './components/navigation-menu-top/navigation-menu-top/navigation-menu-top.component';
+import { NavigationMenuTopComponent } from './components/navigation-menu-top/navigation-menu-top.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,15 @@ import { NavigationMenuTopComponent } from './components/navigation-menu-top/nav
     HeroesComponent,
     HeroDetailComponent,
     NavigationMenuTopComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
